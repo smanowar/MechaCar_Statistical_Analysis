4d27965a0cd42a951a1253b9b2c7a74464c369d5
Farah Manowar <saudiaa.xx@hotmail.com>
2021-08-08 13:10:29 -0400

    deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -23,3 +23,15 @@ total_summary <- suspensionCoil %>% summarize(Mean = mean(PSI), Median = median(
 
 #summary statistics by lot
 lot_summary <- suspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+#--------------
+#Deliverable 3
+#--------------
+
+#PSI all lots
+t.test(suspensionCoil$PSI, mu=1500)
+
+#PSI per lot
+t.test(subset(suspensionCoil,Manufacturing_Lot=="Lot1")$PSI, mu = 1500)
+t.test(subset(suspensionCoil,Manufacturing_Lot=="Lot2")$PSI, mu = 1500)
+t.test(subset(suspensionCoil,Manufacturing_Lot=="Lot3")$PSI, mu = 1500)
