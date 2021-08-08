33cf1ee689791bb3118c201f6dbb947491d240c2
Farah Manowar <saudiaa.xx@hotmail.com>
2021-08-08 12:59:38 -0400

    adding deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,7 +1,7 @@
 library(dplyr)
-
+#----------------
 # Deliverable 1
-
+#----------------
 #read csv into R
 mechaCar <- read.csv("MechaCar_mpg.csv", sep=",", stringsAsFactors = F)
 
@@ -11,3 +11,15 @@ regression_mechaCar
 
 #p value and r^2
 summary(regression_mechaCar)
+
+#--------------
+#Deliverable 2
+#--------------
+#read csv into R
+suspensionCoil <- read.csv("Suspension_Coil.csv", sep=",", stringsAsFactors = F)
+
+#mean, median, and sd
+total_summary <- suspensionCoil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+#summary statistics by lot
+lot_summary <- suspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
